e68ad38699d31e978f74c09e2c90106c6a800d61
shreverr <verma2007s@gmail.com>
2023-05-26 10:15:05 +0530

    added meeting code generation logic

--- src/app/api/meeting/meeting-code/route.ts
+++ src/app/api/meeting/meeting-code/route.ts
@@ -1,17 +1,6 @@
+import generateMeetingCode from '@/lib/meetingCode';
 import { NextResponse } from 'next/server';
- 
-export async function GET() {
-  const res = {
-    name: 'segs'
-  }
- 
-  return NextResponse.json({ res });
-}
 
-export async function POST() {
-  const res = {
-    name: 'fuks'
-  }
- 
-  return NextResponse.json({ res });
+export async function GET() {
+  return NextResponse.json({ meetingCode: generateMeetingCode() });
 }
--- src/lib/meetingCode.ts
+++ src/lib/meetingCode.ts
@@ -1,9 +1,12 @@
-const characters = "abcdefghijklmnopqrstuvwxyz";
+const characters: string = "abcdefghijklmnopqrstuvwxyz";
 
-export default function generateMeetingCode() : string {
-  let generatedMeetingCode = "";
-  for (let index = 0; index < characters.length; index++) {
-    
+export default function generateMeetingCode(): string {
+  let generatedMeetingCode: string = "";
+  const meetingCodeLength: number = 10;
+
+  for (let index = 0; index < meetingCodeLength; index++) {
+    let randomSelectedCharacter: string = characters.charAt(Math.floor(Math.random() * characters.length));
+    generatedMeetingCode += randomSelectedCharacter;
   }
 
   return generatedMeetingCode;
